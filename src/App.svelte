1218a413942dc357cd95102771589ea8df266d0d
tantch <ei12019@fe.up.pt>
2021-10-16 16:36:07 +0100

    svelte init

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Router, Link, Route } from "svelte-navigator";
+  import StudiosPage from "./pages/StudiosPage.svelte";
+  import Home from "./pages/Home.svelte";
+</script>
+
+<main>
+  <Router>
+    <h1>Lampreia Viva</h1>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="studios">Studios</Link>
+    </nav>
+    <div>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/studios">
+        <StudiosPage />
+      </Route>
+    </div>
+  </Router>
+</main>
+
+<style>
+</style>
